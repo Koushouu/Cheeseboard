4b645576aa1eda6ee35c0bc688c7a973123c0bcf
Xingyu Ma <xingyustella.ma@gmail.com>
2026-02-14 19:20:38 +0000

    .

--- util/config.py
+++ util/config.py
@@ -1,7 +1,10 @@
 # Trials Info metadata path
 trial_info_path = "/Users/stella/Desktop/PhD/paulsen_lab/Analysis/kou_analysis/cheeseboard-data/trial_info_20260120.csv"
+# Ephys Info metadata path
+ephys_info_path = "/Users/stella/Desktop/PhD/paulsen_lab/Analysis/kou_analysis/cheeseboard-data/trial_info_LFP_20260205.csv"
 # Animals Info metadata path
 animal_info_path = "/Users/stella/Desktop/PhD/paulsen_lab/Analysis/kou_analysis/cheeseboard-data/animal_info_20251220.csv"
+
 # Cheeseboard map
 cheeseboard_map_path = "/Users/stella/Desktop/PhD/paulsen_lab/Analysis/kou_analysis/cheeseboard-data/cheeseboard_map.csv"
 
